3dcaf3c635d637e21344980defe617054dc1956b
Firna <vyrnvyrd@gmail.com>
2023-01-01 16:09:07 +0700

    refactor(pub-kereta): add ssl

--- pub-kereta3.py
+++ pub-kereta3.py
@@ -4,14 +4,18 @@
 
 import paho.mqtt.client as mqtt
 import json
+import ssl
 
 host = '9c9c43cc8b12425fbc8c9bca59aca94c.s2.eu.hivemq.cloud'
 port = 8883
 usrname = 'hivemq.webclient.1672476741794'
 passwrd = 'lXZU<;7PW5degA:y3j.8'
+context = ssl.create_default_context()
 
 client = mqtt.Client("PKereta1")
 client.username_pw_set(usrname, passwrd)
+client.tls_set_context(context)
+client.tls_insecure_set(True)
 client.connect(host, port)
 client.loop_start()
 
